04956c66d88db9faf58aad3901139496f360f4eb
davigomesflorencio <davigomesflorencio@gmail.com>
2022-04-25 21:47:56 -0300

    feat - Adição da BottomNavigationBar

--- lib/src/views/list_audio/list_audio.view.dart
+++ lib/src/views/list_audio/list_audio.view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:music_player/src/models/data_model.dart';
 import 'package:music_player/src/models/mp3_model.dart';
+import 'package:music_player/src/utils/pallete.dart';
+import 'package:music_player/src/utils/text_style.dart';
+import 'package:music_player/src/widgets/bottom_navigation_bar_widget.dart';
 import 'package:music_player/src/widgets/mp3_item.widget.dart';
 
 class ListAudio extends StatefulWidget {
@@ -35,7 +39,7 @@ class _ListAudioState extends State<ListAudio> {
           model.path.contains("/storage/emulated/0/Music/") == false) {
         _dataMp3Model.addModel(model);
 
-        print(song.filePath);
+        // print(song.filePath);
       }
     }
 
@@ -51,22 +55,96 @@ class _ListAudioState extends State<ListAudio> {
 
   @override
   Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("MusicPlayer"),
+        title: const Text(
+          "MusicFlutter",
+          style: TextStyleMusic.kPrimaryBoldTextStyle,
+        ),
+        centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.queue_music,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
+      backgroundColor: Pallete.background,
       body: _dataMp3Model.mp3Files == null
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : ListView.builder(
-              itemCount: _dataMp3Model.mp3Files?.length ?? 0,
-              itemBuilder: (context, index) {
-                return Mp3Item(mp3File: _dataMp3Model.mp3Files![index]);
-              },
+          : Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: 45,
+                      width: _size.width * 0.4,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.play_arrow,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Play",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: _size.width * 0.4,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Icon(Icons.shuffle, color: Colors.white),
+                            Text(
+                              "Shuffle",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: _dataMp3Model.mp3Files?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    return Mp3Item(mp3File: _dataMp3Model.mp3Files![index]);
+                  },
+                )
+              ],
             ),
+      bottomNavigationBar: const MyBottomNavBar(),
     );
   }
 }
---
+++ lib/src/widgets/bottom_navigation_bar_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyBottomNavBar extends StatefulWidget {
+  const MyBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<MyBottomNavBar> createState() => _MyBottomNavBarState();
+}
+
+class _MyBottomNavBarState extends State<MyBottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+      ],
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+    );
+  }
+}
